3e071c18aee880130ce3ee91d3b7dcffbb9d7aae
David <david.irusta@itscordoba.edu.ar>
2025-07-15 01:41:10 -0300

    Django 1

--- crud/persona/models.py
+++ crud/persona/models.py
@@ -1,20 +1,19 @@
 from django.db import models
 
 class Persona(models.Model):
-    """Model definition fos Persona."""
 
-    nombre = models.CharField(verbose_name="Nombre completo", max_length=50)
-    edad = models.IntegerField(verbose_name="Edad")
-    email = models.EmailField(verbose_name="Correo electronico", max_length=254)
+    nombre = models.CharField(verbose_name= "Nombre Completo", max_length=50)
+    edad = models.IntegerField(verbose_name= "Edad")
+    email = models.EmailField(verbose_name="Correo Electronico", max_length=254)
 
     class Meta:
-        """Meta definition fos Persona."""
-
-        verbose_name = 'Persona'
-        verbose_name_plural = 'Personas'
+        verbose_name = ("Persona")
+        verbose_name_plural = ("Personas")
 
     def __str__(self):
-        """Unicode representation os Persona."""
         return f"{self.nombre} - {self.email}"
+
+    '''def get_absolute_url(self):
+        return reverse("Persona_detail", kwargs={"pk": self.pk})'''
 
 
